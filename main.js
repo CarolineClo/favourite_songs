42a1aa9730d73d280b04fd32a2d58694b9a0dbd1
Caroline <caro672g@stud.kea.dk>
2022-02-06 00:16:44 +0100

    Initial

---
+++ main.js
@@ -0,0 +1,61 @@
+const favourite = [
+  {
+    name: "All My Friends",
+    artist: "LCD Sound System",
+    memory: "Pointing at my friends faces on the dancfloor",
+  },
+
+  {
+    name: "Good Fortune",
+    artist: "PJ Harvey",
+    memory: "swinging my handbag",
+  },
+
+  {
+    name: "Annies Song",
+    artist: "John Denver",
+    memory: "Mum",
+  },
+
+  {
+    name: "Do you realise?",
+    artist: "The flaming lips",
+    memory: "That one pub near the Art School",
+  },
+
+  {
+    name: "Elliott Smith",
+    artist: "Between the Bars",
+    memory: "Travelling around Australia",
+  },
+
+  {
+    name: "Buffalo Stance",
+    artist: "Neneh Cherry",
+    memory: "Long car rides",
+  },
+
+  {
+    name: "No Children",
+    artist: "The Mountain goats",
+    memory: "The Cowboy, tipping his hat.",
+  },
+
+  {
+    name: "Cannonball",
+    artist: "The Breeders",
+    memory: "Third year",
+  },
+
+  {
+    name: "I love to hate you",
+    artist: "Erasure",
+    memory: "Every single breakup ever",
+  },
+
+  {
+    name: "Get Free",
+    artist: "Major lazer",
+    memory: "That last year in Scotland",
+  },
+];
